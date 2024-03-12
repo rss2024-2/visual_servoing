e1b046c71575f0550eae22a929721633d5d50d15
ningshan <ningshan@mit.edu>
2024-03-12 02:11:28 +0000

    color segmentation v1

--- visual_servoing/visual_servoing/computer_vision/color_segmentation.py
+++ visual_servoing/visual_servoing/computer_vision/color_segmentation.py
@@ -13,6 +13,10 @@ import numpy as np
 #  v
 ###############################################################
 
+lower_orange = np.array([0, 100, 100])
+high_orange = np.array([20, 255, 255])
+MIN_CONTOUR_AREA = 100
+
 def image_print(img):
 	"""
 	Helper function to print out images, for debugging. Pass them in as a list.
@@ -33,10 +37,28 @@ def cd_color_segmentation(img, template):
 				(x1, y1) is the top left of the bbox and (x2, y2) is the bottom right of the bbox
 	"""
 	########## YOUR CODE STARTS HERE ##########
+	# Convert the image to HSV
+	hsv_img = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+	#use cv2.inRange to apply a mask to the image
+	color_mask = cv2.inRange(hsv_img, lower_orange, high_orange)
+	img_masked = cv2.bitwise_and(hsv_img,hsv_img, mask=color_mask)
+	
+	bgr_img_masked = cv2.cvtColor(img_masked, cv2.COLOR_HSV2BGR)
 
-	bounding_box = ((0,0),(0,0))
+	# Apply thresholding to the masked image
+	_, thresholded_image = cv2.threshold(bgr_img_masked, 40, 255, cv2.THRESH_BINARY)
+	contours, _  = cv2.findContours(thresholded_image, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
+	if len(contours) > 0:
+                best_contour = max(contours, key=cv2.contourArea) # Choose contour of largest area
+                if cv2.contourArea(best_contour) >= MIN_CONTOUR_AREA: # Super small contour --> likely just noise
+                    # Build Bounding Box
+                    x,y,w,h = cv2.boundingRect(best_contour)
+                    bounding_box = ((x,y), (x+w, y+h))
+                    return bounding_box
+    
+
 
 	########### YOUR CODE ENDS HERE ###########
 
 	# Return bounding box
-	return bounding_box
